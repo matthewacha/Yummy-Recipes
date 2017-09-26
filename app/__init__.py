68b8672df91a22db8fd25d22d2ef3087a1b1b7be
All Eyes <matthewacha@gmail.com>
2017-09-26 10:31:58 +0300

    edit __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_required
 
 app = Flask(__name__)
 
+app = Flask(__name__, instance_relative=True)
+app.config.from_object('config.Developer')
 
+yummy = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_message = 'You must be logged in to view this'
+login_manager.view='user.login'
 
 from app import models
 
