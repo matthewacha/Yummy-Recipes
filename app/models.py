ab5eeb305052a330ef59e3ed3fbfcf7163c268ca
All Eyes <matthewacha@gmail.com>
2017-10-03 15:48:47 +0300

    Update models

--- app/models.py
+++ app/models.py
@@ -1,75 +1,119 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy import create_engine
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
-from sqlalchemy.orm import relationship
-from app import yummy, login_manager
 
 
-class User(yummy.Model, UserMixin):
-    __tablename__ = 'user'
-    id = yummy.Column(yummy.Integer, primary_key=True)
-    first_name = yummy.Column(yummy.String(60))
-    last_name = yummy.Column(yummy.String(60))
-    email = yummy.Column(yummy.String(60), unique=True)
-    password_hash = yummy.Column(yummy.String(300))
-    category_id = yummy.Column(yummy.Integer, yummy.ForeignKey('categories.id'))
-    def __init__(self, first_name, last_name, email, password):
+class User(object):
+    def __init__(self, first_name, last_name, email, password, category_name,category_description,
+                 users=[]):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.password = password
-    @property
-    def password(self):
-        #keep password private
-        raise AttributeError('password is not a readable attribute.')  
+        self.users = users
+        self.category_name = category_name
+        self.category_description = category_description
    
-    @password.setter  
-    def password(self, password):
+    def password_hsh(self, password):
        #generate password_hash
-        self.password_hash = generate_password_hash(password) 
+        self.password_hash = generate_password_hash(self.password)
 		
     def verify_password(self, password):
         #check password with password_hash
         return check_password_hash(self.password_hash, password)
+    
+    def add_user(self):
+        #creates a new user
+        self.users.append(
+            {
+                'first_name':self.first_name,
+                'last_name':self.last_name,
+                'email':self.email,
+                'password':self.password_hsh(self.password),
+                'categories':{},
+                'recipes':{}
+            }
+        )
+        return self.users
+    
+    def find_user(self,email):
+        #gets a user from the database
+        for user in self.users:
+            for key in user.keys():
+                if user['email'] == self.email:
+                    return user
+                else:
+                    return 'user doesnt exist'
 
-    def __repr__(self):
-        return '{}'.format(self.first_name)
-  
-    #setup user loader  
-    @login_manager.user_loader
-    def get_user(id):
-        return User.query.get(int(id))
- 
-class Category(yummy.Model):
-    __tablename__ = 'categories'
-    id = yummy.Column(yummy.Integer, primary_key=True)
-    name = yummy.Column(yummy.String(60), nullable=False)
-    description = yummy.Column(yummy.String(200), nullable=False)
-    recipe_id=yummy.Column(yummy.Integer, ForeignKey('recipes.id'))
-    user = yummy.relationship('User', backref='categories',
-                                 lazy='dynamic')
+    def list_user(self):
+        for user in self.users:
+            return user
+
+    def add_category(self):
+        #add category
+        for user in self.users:
+            for key in user.keys():
+                if key == 'categories':
+                    user['categories'] = {self.category_name:self.category_description}
+                    return user['categories']
+    def find_category(self):
+        for user in self.users:
+            for key in user.keys():
+                if key == 'categories':
+                    return user['categories'][self.category_name]
+                
+class Category:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-    def __repr__(self):
-        return '{} {}'.format(self.name, self.description)
+        
+    def add_category(self):
+        for user in user_db:
+            return user
+            for key in user.keys():
+                if user['categories']:
+                    user['categories'] = {self.name:self.description}
+                    return user['categories']
+                    
+    def find_category(self, name):
+        for user in users:
+            for key in user.iterKeys():
+                if key == 'categories':
+                    for key in user['categories'].iterKeys():
+                        if key == self.name:
+                            return key
+                        
+    def delete_category(self, name):
+        user.pop(User.find_category(self.name))
+         
+    def show_categories(self):
+        for user in users:
+            for key in user.iterKeys():
+                if key == 'categories':
+                    return key
 		
-class Recipe(yummy.Model):
-    __tablename__ = 'recipes'	
-    id = yummy.Column(yummy.Integer, primary_key=True)
-    name = yummy.Column(yummy.String(100), nullable=False)
-    description = yummy.Column(yummy.String(500), nullable=False)
-    category = yummy.relationship('Category', backref='recipes', \
-	                               lazy='dynamic')
-    def __init__(self, name, description):
+class Recipe(User):
+    def __init__(self, name, description, category_name):
         self.name = name 
         self.description = description
-    def __repr__(self):
-        return '{}'.format(self.name)	
 
-    #create engine to store data in local non-persistent database directory   
+        def add_recipe(self, name, description):
+            for user in users:
+                for key in user.iterKey:
+                    if key == 'recipes':
+                        user['recipes'] = {[self.name, self.description]:self.category_name}
 
-yummy.Model.metadata.bind = create_engine('sqlite:///:memory:') 
-
-yummy.create_all()
+        def find_recipe(self, name):
+            for user in users:
+                for key in user.iterKeys():
+                    if key == 'recipes':
+                        recipe_info = user['recipes']
+                        for k in recipe_info.iterKeys(): 
+                            if  k[0] == self.name:
+                                return k
+        def delete_recipe(self, name):
+            for user in users:
+                for key in user.iterKeys():
+                    if key == 'recipes':
+                        recipe_info = user['recipes']
+                        for k in recipe_info.iterKeys(): 
+                            if  k[0] == self.name:
+                                del recipe_info[k[0]]
